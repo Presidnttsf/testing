23124abbc051c8f9a34d3b3a623f7377ef2db458
tauseef akhtar <presidnttsf@gamil.comr>
2021-12-21 01:40:20 +0530

    added greeting function

--- func/power-of-num.js
+++ func/power-of-num.js
@@ -1,4 +1,9 @@
-
+/**
+ * 
+ * @param {number} a its a simple number 
+ * @param {number} b power of number 
+ * @returns number which is power of  number a
+ */
 export function PON(a, b) {
     let result = 1;
     for (let index = 1; index <= b; index++) {
