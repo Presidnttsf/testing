c82ca0c124f19d390a25306abd214e3c5616576c
anwar069 <anwar069@gmail.com>
2021-12-21 01:17:12 +0530

    Added documentation for Triangle Area

--- func/areaOfTriangle.js
+++ func/areaOfTriangle.js
@@ -1,3 +1,9 @@
+/**
+ * 
+ * @param {Number} w Widht of triangle
+ * @param {Number} h Height of triangle
+ * @returns Number Area of the triangle based on height and Widht
+ */
 export function areaofTriangle(w, h) {
     return 0.5 * w * h;
 }
