5134f77878095545654e6a197543e47e3cc562e0
anwar069 <anwar069@gmail.com>
2021-12-21 00:52:13 +0530

    updated comments

--- func/leapYear.js
+++ func/leapYear.js
@@ -1,14 +1,19 @@
+// if year is divisible by 400 then is_leap_year
+// else if year is divisible by 100 then not_leap_year
+// else if year is divisible by 4 then is_leap_year
+// else not_leap_year
+
 export function leapYear(a) {
 
-    let ly=a%4;
+  let ly = a % 4;
 
-    if (ly==0) {
-     console.log ("then it is leap year")
-        
-    } else {
-      console.log( "it is not leap year")
-    }
-    return ly ;
-    
+  if (ly == 0) {
+    console.log("then it is leap year")
+
+  } else {
+    console.log("it is not leap year")
+  }
+  return ly;
+
 }
 
