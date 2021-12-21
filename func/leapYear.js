cfbdc644bbd5d19857469c51ff68a452253c9f01
Mohd zafar aalam <dildar88887>
2021-12-22 00:46:18 +0530

    leap year function

---
+++ func/leapYear.js
@@ -0,0 +1,14 @@
+export function leapYear(a) {
+
+    let ly=a%4;
+
+    if (ly==0) {
+     console.log ("then it is leap year")
+        
+    } else {
+      console.log( "it is not leap year")
+    }
+    return ly ;
+    
+}
+
--- zafar.js
+++ zafar.js
@@ -1,2 +1,4 @@
 import { areaOfRectangle } from "./func/area-of-recatangle.js";
+import { leapYear } from "./func/leapYear.js";
 console.log(areaOfRectangle(2,3));
+console.log(leapYear(2019));
