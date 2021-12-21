86a5590781988dda0b81d85f36616e3086168327
qasim q <qasim007officail@gmail.com>
2021-12-22 03:09:23 +0530

    a

--- qasim.js
+++ qasim.js
@@ -1,7 +1,16 @@
 ///////area of circle vs area od square
 
-
-export function aoc(d) {
-    return 3.14 * 0.25 * d * d
+export function aocVsAos(params) {
 
 }
+
+function aoc(d) {
+    return 3.14 * 0.25 * d * d;
+}
+console.log(aoc(3))
+
+function aos(a) {
+    return a * a
+}
+console.log(aos(5))
+
