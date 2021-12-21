7b2baf18fcff1603993b89dbcf9ab4f13043d20b
qasim q <qasim007officail@gmail.com>
2021-12-22 03:47:06 +0530

    extra coffe

--- qasim.js
+++ qasim.js
@@ -1,37 +1,37 @@
-///////area of circle vs area od square
+// ///////area of circle vs area od square
 
-export function aocVsAos(x) {
-
-
-    function aoc(d) {
-        let ans1 = 3.14 * 0.25 * d * d;
-        return ans1;
-    }
-    console.log(aoc(3));
-
-    function aos(a) {
-        let ans2 = a * a;
-        return ans2;
-    }
-    console.log(aos(5))
-
-    if (ans1 > ans2) {
-        return aoc;
-
-    } else {
-
-        return aos;
-
-    }
-
-}
-
-console.log(aocVsAos());
+// export function aocVsAos(x) {
 
 
+//     function aoc(d) {
+//         let ans1 = 3.14 * 0.25 * d * d;
+//         return ans1;
+//     }
+//     console.log(aoc(3));
 
-function acVsAs(params) {
+//     function aos(a) {
+//         let ans2 = a * a;
+//         return ans2;
+//     }
+//     console.log(aos(5))
+
+//     if (ans1 > ans2) {
+//         return aoc;
+
+//     } else {
+
+//         return aos;
+
+//     }
+
+// }
+
+// console.log(aocVsAos());
 
 
 
-}
+// function acVsAs(params) {
+
+
+// }
+
