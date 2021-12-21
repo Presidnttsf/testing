fc42e6e88fdc26d8e38b77ec2eab0acd3eb26ad4
qasim q <qasim007officail@gmail.com>
2021-12-22 02:46:17 +0530

    zzzzzzzzzz

--- qasim.js
+++ qasim.js
@@ -1,11 +1,1 @@
-import { areaofTriangle } from "./func/areaOfTriangle.js";
-import { areaofsquar } from "./func/areofsquar.js";
-import { switchcombinations } from "./func/switchcombination.js";
-
-
-
-// console.log(areaofTriangle(4, 5))
-
-
-console.log(areaofsquar(5))
-console.log(switchcombinations(6));
+dfbdfjdsjbdmsbgamsggrsfg
