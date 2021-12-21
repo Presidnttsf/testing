6cb366da363d633c0577f04768fa37b5b556c841
Tarique Ahmed <tariqueahmed95597@gmail.com>
2021-12-22 04:36:19 +0530

    circle vs square function

---
+++ func/circle-Vs-Square.js
@@ -0,0 +1,17 @@
+
+
+
+export function cirVsSqr(a) {
+    let result1 = 3.14 * a * a;
+    return result1;
+
+    let result2 = a * a;
+    return result2;
+
+
+    if (result1 > result2) {
+        return result1;
+    } else {
+        return result2;
+    }
+}
