c20043044daf428cccb84e026af4ae36cb2398e8
Wajid <malikwajid11@gmail.com>
2021-12-23 00:48:54 +0530

    ........

--- wajid.js
+++ wajid.js
@@ -1,5 +1,10 @@
 import { numberBetween } from "./func/numberbetween.js";
 import { numberIsEven } from "./func/numberEven.js";
+import { numberIsOdd } from "./func/numberIsOdd.js"
+import { isPrime } from "./func/find-prime-num.js"
+
+
 import { switchcombinations } from "./func/switchcombination.js";
 // console.log(numberBetween(1, 10))
-console.log(switchcombinations(8));
+console.log(isPrime(10));
+
