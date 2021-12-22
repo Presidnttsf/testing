5b165159ab9b683b79b989116c9cfb098bd7c654
qasim q <qasim007officail@gmail.com>
2021-12-22 10:32:09 +0530

    .....

--- func/extra-cofee.js
+++ func/extra-cofee.js
@@ -3,4 +3,4 @@ export function coffeCup(x) {
     return x * 0.25
 
 }
-console.log(coffeCup(8), 'coffe is extra  thank you  visit again')          
+console.log(coffeCup(20), 'coffe is extra  thank you  visit again')          
