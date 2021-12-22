a75e4f2334d4409873e7ba22053add51e7ec705d
qasim q <qasim007officail@gmail.com>
2021-12-23 02:41:44 +0530

    .

--- qasim.js
+++ qasim.js
@@ -1,5 +1,7 @@
 // ///////area of circle vs area od square
 
+// import { getOperation } from "./func/get-Operation"
+
 // export function aocVsAos(x) {
 
 
@@ -34,4 +36,9 @@
 
 
 // }
+// import { getOperation } from "./func/get-Operation.js";
+// console.log(getOperation(5, 3, 2));
 
+// import { coffeCup } from "./func/extra-cofee.js";
+
+// console.log(coffeCup(10));
