a8d1743f2b663e425d1953eea753483f68917452
anwar069 <anwar069@gmail.com>
2021-12-22 23:20:10 +0530

    updated PON

--- tauseef.js
+++ tauseef.js
@@ -1,9 +1,9 @@
-import { PON } from "./func/power-of-num.js";
+import { PowerOfNumber } from "./func/power-of-num.js";
 import { isSum } from "./func/is-sum-n.js";
 import { Greet } from "./func/name.greet.js";
 import { areaofTriangle } from "./func/areaofTriangle.js";
 import { isPrime } from "./func/find-prime-num.js";
-console.log(PON(2, 10));
+console.log(PowerOfNumber(2, 10));
 // console.log(isSum(2, 3, 5));
 // console.log(Greet("Tauseef", 23));
 console.log(areaofTriangle(20, 40));
