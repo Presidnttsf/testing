4503fb2e01d0fe1bb563616e0399c860e066e4b4
qasim q <qasim007officail@gmail.com>
2021-12-29 00:46:24 +0530

    bbbbbbbbbbbbb

--- func/find-prime-num.js
+++ func/find-prime-num.js
@@ -2,7 +2,6 @@
 export function isPrime(a) {
     for (let index = 2; index < a; index++) {
         let num = a % index
-        console.log(num, index, a)
         if (num == 0) {
             return false
         }
