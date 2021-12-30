c39b22a63db0e69b0f7a2950cdcc734dde883ece
qasim q <qasim007officail@gmail.com>
2021-12-30 23:03:04 +0530

    2

--- qasim.js
+++ qasim.js
@@ -1,2 +1,12 @@
-import { numberBetweenPrime } from "./func/num-between-prime.js";
-console.log(numberBetweenPrime(10, 15))
+// import { areaOfRectangle } from "./func/area-of-recatangle.js";
+// console.log(areaOfRectangle(10, 20))
+
+
+
+// import { numberBetweenPrime } from "./func/num-between-prime.js";
+// import { abc } from "./func/temp.js";
+// import { RotateArray } from "./func/rotate-array.js";
+// console.log(RotateArray([3, 6, 9, 5, 5]))
+
+
+
