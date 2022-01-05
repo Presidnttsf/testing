7f56e1882b92b84cd04a6abecd77376005d76607
anwar069 <anwar069@gmail.com>
2022-01-06 00:14:33 +0530

    Added prompt test

--- ahmed.js
+++ ahmed.js
@@ -2,6 +2,7 @@ import { sum } from "./func/sum-two-num.js";
 import { areaofTriangle } from "./func/areaOfTriangle.js";
 import { areaOfRectangle } from "./func/area-of-recatangle.js";
 import { getkthMax } from "./func/get-kth-max-from-array.js";
+import { promptTest } from "./func/prompt-test.js";
 
 
-console.log(getkthMax([5, 8, 9, 7, 6, 2], 4))
+promptTest();
