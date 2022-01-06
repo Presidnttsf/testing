654de97eacd97ee0b38ea460ea45a1c05c3309aa
tauseef akhtar <presidnttsf@gamil.comr>
2022-01-07 00:24:17 +0530

    sim check updated

---
+++ func/sim-check.js
@@ -0,0 +1,15 @@
+import prompt_sync from "prompt-sync";
+const prompt = prompt_sync({ sigint: true });
+
+export function simCheck() {
+    const count = prompt("Enter your mobile number: ");
+    console.log('Enter your mobile number: ', count);
+    if (count.startsWith(90))
+        return "JIO"
+
+    if (count.startsWith(93))
+        return "Reliance"
+
+    if (count.startsWith(94))
+        return "Airtel"
+}   
