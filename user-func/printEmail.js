d703fade141cde2c87f53fc8326b46b089e99b90
Mohd zafar aalam <dildar88887@gmail.com>
2022-01-13 23:18:04 +0530

    .com print email

--- user-func/printEmail.js
+++ user-func/printEmail.js
@@ -1,4 +1,10 @@
-export function printEmail(users, domain) {
-
-
+export function printEmail(users, email) {
+    const arr = [];
+    for (let index = 0; index < users.length; index++) {
+        const element = users[index];
+        if (element.email.endsWith(email)) {
+            arr.push(element);
+        }
+    }
+    return arr;
 }
