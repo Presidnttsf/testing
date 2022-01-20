dc3a21a92cafdc5d73224fe0efd3e3abd07206d0
tauseef akhtar <presidnttsf@gamil.comr>
2022-01-21 03:01:59 +0530

    prompt bill updated

--- func/promptBill.js
+++ func/promptBill.js
@@ -4,22 +4,31 @@ const prompt = prompt_sync({ sigint: true });
 export function printBill() {
     let count = prompt('Enter the number of product: ')
     console.log('count products number :', count);
+    let collectionOfProduct = [];
     let productTotal = 0;
+    let discount = 0;
     for (let index = 0; index < count; index++) {
         const productName = prompt('Enter name of product: ' + (index + 1) + ": ");
         const productPrice = prompt('Enter price of product: ' + (index + 1) + ": ");
         productTotal += Number(productPrice)
-
+        collectionOfProduct.push(productName, productPrice)
 
 
     }
     let gst = productTotal * 0.18
     let totalWithGst = productTotal + gst
-    console.log('productTotal: ', productTotal.toFixed(2));
-    console.log('gst:', gst.toFixed(2));
-    console.log('totalWithGst: ', totalWithGst.toFixed(2));
+    if (productTotal >= 5000) {
+        discount = totalWithGst - (productTotal * 0.10)
+    }
 
 
 
+    console.log(collectionOfProduct)
+    console.log('Product Total = ', 'RS.', productTotal.toFixed(2));
+    console.log('GST 18% =', 'RS.', gst.toFixed(2));
+    console.log('Total Amount =', 'RS.', totalWithGst.toFixed(2));
+    console.log('Discount =', 'RS.', (productTotal * 0.10).toFixed(2))
+    console.log('Discounted Total =', 'RS.', discount);
 
 }
+
